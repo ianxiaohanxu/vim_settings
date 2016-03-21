591c5d4db09333c1ac70167af891a558cc94b4b4
Alex <ianxiaohanxu@163.com>
2016-03-21 11:49:47 +0800

    update stucture

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cp ./vimrc ~/.vimrc
-mkdir -p ~/.vim/ftplugin
-cp ./python.vim ~/.vim/ftplugin/
+if [ ! -d ~/.vim ];then
+    mkdir -p ~/.vim
+fi
+cp -r ./ftplugin ~/.vim/
+cp -r ./ftdetect ~/.vim/
+cp -r ./after ~/.vim/
